e8eb07185ce5d1f69e05218ffa968d875075e97c
cpedrohenrique1 <88724009+cpedrohenrique1@users.noreply.github.com>
2023-03-12 22:06:39 -0300

    Update hello.asm

--- HelloWorld/hello.asm
+++ HelloWorld/hello.asm
@@ -7,13 +7,13 @@ global _start
 _start: ; rotina principal
 
 imprimir:
-  mov eax, 0x4 ; código de chamada para sys_write
-  mov ebx, 0x1 ; código de chamada para stdout
-  mov ecx, mens ; endereço da mensagem
+  mov eax, 0x4 ; código de chamada para sys_write < 4 ->(serviço print)
+  mov ebx, 0x1 ; código de chamada para stdout < 1 ->(FD da tela)
+  mov ecx, mens ; endereço da mensagem (ponteiro da origem)
   mov edx, tam ; tamanho da mensagem
   int 0x80 ; chama o sistema
 
 fim:
-  mov eax, 0x1 ; código de chamada para sys_exit
+  mov eax, 0x1 ; código de chamada para sys_exit < 1 ->(serviço exit)
   mov ebx, 0x0 ; código de chamada para sucesso (retorna 0)
   int 0x80 ; chama o sistema
