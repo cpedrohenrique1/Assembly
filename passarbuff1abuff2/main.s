d94f7c6c8cd1d9bda97921e614bde0c92222bf78
cpedrohenrique1 <88724009+cpedrohenrique1@users.noreply.github.com>
2023-03-27 09:45:23 -0300

    Add files via upload

--- passarbuff1abuff2/main.s
+++ passarbuff1abuff2/main.s
@@ -1,58 +1,59 @@
 segment .data
 mens db "Digite uma frase:", 0xA
-mens2 db "A frase digitada foi: ", 0xA
 tam equ $-mens
+mens2 db "A frase digitada foi: ", 0xA
 tam2 equ $-mens2
 
-section .bss
+segment .bss
 buff1 resb 50
 buff2 resb 50
 qde resd 1
 
-section .text
+segment .text
 global _start
 _start:
 
-print1:
-mov eax, 0x4
-mov ebx, 0x1 ; FD da Tela
+print:
+mov eax, 4
+mov ebx, 1 ; FD da Tela
 mov ecx, mens
 mov edx, tam
 int 0x80
 
 read:
-mov eax, 0x3
-mov ebx, 0x0 ; FD do teclado
+mov eax, 3
+mov ebx, 0 ; FD do teclado
 mov ecx, buff1
 mov edx, 50
 int 0x80
-mov [qde], eax
 
+mov [qde], eax
 xor esi, esi ; esi = 0
 
 iniloop:
-mov al, [buff1 + esi] ; al recebe buff1 na posição 0
+mov al, [buff1 + esi] ; al recebe buff1 na posição 0 / [base + indice]
 mov [buff2 + esi], al ; buff2 na posição 0 vai receber al
 inc esi ; incrementa 1 em esi = esi++
 cmp esi,[qde] ; compara esi com qde (quantidade de caracteres digitados)
 je print2 ; se for o mesmo tamanho vai para print2
 jmp iniloop ; se nao volta para o loop
+int 0x80
 
 print2:
-mov eax, 0x4
-mov ebx, 0x1
+mov eax, 4
+mov ebx, 1
 mov ecx, mens2
 mov edx, tam2
 int 0x80
 
 print3:
-mov eax, 0x4
-mov ebx, 0x1
+mov eax, 4
+mov ebx, 1
 mov ecx, buff2
 mov edx, qde
 int 0x80
 
 fim:
-mov eax, 0x1
-mov ebx, 0x0
+mov eax, 1
+mov ebx, 0
 int 0x80
