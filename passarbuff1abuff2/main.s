8a4f2337e85cc9c7f72a2fef1f5dba816e55e342
cpedrohenrique1 <88724009+cpedrohenrique1@users.noreply.github.com>
2023-03-27 08:39:16 -0300

    Add files via upload

---
+++ passarbuff1abuff2/main.s
@@ -0,0 +1,58 @@
+segment .data
+mens db "Digite uma frase:", 0xA
+mens2 db "A frase digitada foi: ", 0xA
+tam equ $-mens
+tam2 equ $-mens2
+
+section .bss
+buff1 resb 50
+buff2 resb 50
+qde resd 1
+
+section .text
+global _start
+_start:
+
+print1:
+mov eax, 0x4
+mov ebx, 0x1 ; FD da Tela
+mov ecx, mens
+mov edx, tam
+int 0x80
+
+read:
+mov eax, 0x3
+mov ebx, 0x0 ; FD do teclado
+mov ecx, buff1
+mov edx, 50
+int 0x80
+mov [qde], eax
+
+xor esi, esi ; esi = 0
+
+iniloop:
+mov al, [buff1 + esi] ; al recebe buff1 na posição 0
+mov [buff2 + esi], al ; buff2 na posição 0 vai receber al
+inc esi ; incrementa 1 em esi = esi++
+cmp esi,[qde] ; compara esi com qde (quantidade de caracteres digitados)
+je print2 ; se for o mesmo tamanho vai para print2
+jmp iniloop ; se nao volta para o loop
+
+print2:
+mov eax, 0x4
+mov ebx, 0x1
+mov ecx, mens2
+mov edx, tam2
+int 0x80
+
+print3:
+mov eax, 0x4
+mov ebx, 0x1
+mov ecx, buff2
+mov edx, qde
+int 0x80
+
+fim:
+mov eax, 0x1
+mov ebx, 0x0
+int 0x80
