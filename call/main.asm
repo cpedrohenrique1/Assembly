f36c3abd49581bb6e8519b8fe48b10eb85fd58a8
cpedrohenrique1 <88724009+cpedrohenrique1@users.noreply.github.com>
2023-04-03 09:25:38 -0300

    Add files via upload

---
+++ call/main.asm
@@ -0,0 +1,72 @@
+segment .data
+mens1 db "Insira a frase: ", 0xA
+tam1 equ $-mens1
+mens2 db "A letra u esta na mensagem digitada", 0xA
+tam2 equ $-mens2
+mens3 db "A letra u nao esta na mensagem inserida", 0xA
+tam3 equ $-mens3
+var dw 0
+
+segment .bss
+entrada resb 50
+qde resd 1
+
+segment .text
+
+read:
+mov eax,3
+mov ebx,0
+int 0x80
+ret
+
+print:
+mov eax, 4
+mov ebx, 1
+int 0x80
+ret
+
+global _start
+_start:
+
+printmens1:
+mov ecx,mens1
+mov edx,tam1
+call print
+
+readentrada:
+mov ecx,entrada
+mov edx,50
+call read
+mov [qde],eax
+
+xor esi,esi
+
+verificarletra:
+mov al,[entrada + esi]
+cmp al, "u"
+inc var
+cmp esi, [qde]
+je testarvar
+jmp verificarletra
+int 0x80
+
+testarvar:
+cmp var, 0
+je printmens3
+jmp printmens2
+
+printmens3:
+mov ecx,mens3
+mov edx,tam3
+call print
+jmp fim
+
+printmens2:
+mov ecx,mens2
+mov edx,tam2
+call print
+
+fim:
+mov eax,1
+mov ebx,0
+int 0x80
